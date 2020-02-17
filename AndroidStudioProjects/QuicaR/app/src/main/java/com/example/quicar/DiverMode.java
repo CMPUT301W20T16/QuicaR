59fe3006d016d3309dd90db650ef82920bfc2da9
PANGK4IK4I <wenkai1@ualberta.ca>
2020-02-17 16:56:44 -0700

    update user part

--- AndroidStudioProjects/QuicaR/app/src/main/java/com/example/quicar/DiverMode.java
+++ AndroidStudioProjects/QuicaR/app/src/main/java/com/example/quicar/DiverMode.java
@@ -1,4 +1,26 @@
 package com.example.quicar;
 
 public class DiverMode {
+    private boolean open = false;
+    private Request request = null;
+
+    protected void openMode(){
+        open = true;
+    }
+
+    protected void closeMode(){
+        open = false;
+    }
+
+    protected boolean isMode(){
+        return open;
+    }
+
+    protected void acceptReq(Request newRequest){
+        request = newRequest;
+    }
+
+    protected Request getReq(){
+        return request;
+    }
 }
--- AndroidStudioProjects/QuicaR/app/src/main/java/com/example/quicar/RiderMode.java
+++ AndroidStudioProjects/QuicaR/app/src/main/java/com/example/quicar/RiderMode.java
@@ -1,4 +1,24 @@
 package com.example.quicar;
 
 public class RiderMode {
+    private boolean open = true;
+    private Request request = null;
+
+    protected void openMode(){
+        open = true;
+    }
+
+    protected void closeMode(){
+        open = false;
+    }
+
+    protected boolean isMode(){
+        return open;
+    }
+
+    protected void makeRequest(Location start, Location destination){
+        request = new Request();
+        //request.setStart(start);
+        //request.setDestination(destination);
+    }
 }
