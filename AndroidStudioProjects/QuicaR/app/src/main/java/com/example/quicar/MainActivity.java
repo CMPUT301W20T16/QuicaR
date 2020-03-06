2a5aa9d775134350cc212be096fe41d32fd4c6b8
PANGK4IK4I <wenkai1@ualberta.ca>
2020-03-06 13:25:34 -0700

    Merge branches 'YouweiMushroom' and 'pangkaikai' of https://github.com/CMPUT301W20T16/QuicaR into pangkaikai
    # Conflicts:
    #	AndroidStudioProjects/QuicaR/app/src/main/java/com/example/quicar/DiverMode.java
    #	AndroidStudioProjects/QuicaR/app/src/main/java/com/example/quicar/DriverInfo.java
    #	AndroidStudioProjects/QuicaR/app/src/main/java/com/example/quicar/RiderMode.java

--- AndroidStudioProjects/QuicaR/app/src/main/java/com/example/quicar/MainActivity.java
+++ AndroidStudioProjects/QuicaR/app/src/main/java/com/example/quicar/MainActivity.java
@@ -6,24 +6,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
 
     Button scanButton;
+    String test = "User: Pangkaikai\n Driver: PANGKAIKAI\n Start: U of A\nDestination: Windsor Park Plaza\nTime: 13:13\nCost: $20";
+    ImageView imageView;
 
     @Override
     protected  void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.driver_scan);
+        setContentView(R.layout.activity_main);
 
         scanButton = (Button) findViewById(R.id.scan_button);
+        imageView = (ImageView) findViewById(R.id.qr_code);
 
+        /*
         scanButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(), ScanActivity.class));
+            }
+        });
+        */
+
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new QR(imageView).execute("https://api.qr-code-generator.com/v1/create/" + test);
             }
         });
     }
--- AndroidStudioProjects/QuicaR/app/src/main/java/com/example/quicar/QR.java
+++ AndroidStudioProjects/QuicaR/app/src/main/java/com/example/quicar/QR.java
@@ -1,4 +1,61 @@
 package com.example.quicar;
 
-public class QR {
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.ref.WeakReference;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class QR extends AsyncTask<String, Void, Bitmap> {
+    private final WeakReference<ImageView> imageViewWeakReference;
+
+    public QR(ImageView imageView){
+        imageViewWeakReference = new WeakReference<>(imageView);
+    }
+    @Override
+    protected Bitmap doInBackground(String... strings) {
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Bitmap bitmap){
+        if (isCancelled()){
+            bitmap = null;
+        }
+        ImageView imageView = imageViewWeakReference.get();
+        if (imageView != null){
+            if (bitmap != null){
+                imageView.setImageBitmap(bitmap);
+            }
+        }
+    }
+
+    private Bitmap getBitmap(String url){
+        HttpURLConnection urlConnection = null;
+        try{
+            URL myUrl = new URL(url);
+            urlConnection = (HttpURLConnection) myUrl.openConnection();
+            int statusCode = urlConnection.getResponseCode();
+            if (statusCode != HttpURLConnection.HTTP_OK){
+                return null;
+            }
+            InputStream inputStream = urlConnection.getInputStream();
+            if (inputStream != null){
+                return BitmapFactory.decodeStream(inputStream);
+            }
+        } catch (Exception e) {
+            urlConnection.disconnect();
+        } finally {
+            if (urlConnection != null){
+                urlConnection.disconnect();
+            }
+        }
+        return null;
+    }
 }
