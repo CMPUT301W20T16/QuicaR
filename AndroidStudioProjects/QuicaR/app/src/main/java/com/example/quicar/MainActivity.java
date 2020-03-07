9ef74574f379adb1ee45b13078b37c60317087ff
PANGK4IK4I <wenkai1@ualberta.ca>
2020-03-07 12:20:52 -0700

    update

--- AndroidStudioProjects/QuicaR/app/src/main/java/com/example/quicar/MainActivity.java
+++ AndroidStudioProjects/QuicaR/app/src/main/java/com/example/quicar/MainActivity.java
@@ -2,42 +2,31 @@ package com.example.quicar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
+import java.time.LocalDateTime;
 
-import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
 
 public class MainActivity extends AppCompatActivity {
 
     Button scanButton;
-    String test = "User: Pangkaikai\n Driver: PANGKAIKAI\n Start: U of A\nDestination: Windsor Park Plaza\nTime: 13:13\nCost: $20";
+    String test = "User: Pangkaikai\nDriver: PANGKAIKAI\nStart: U of A\nDestination: Windsor Park Plaza\nTime: 13:13\nCost: $20";
     ImageView imageView;
 
     @Override
     protected  void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.rider_qr);
 
-        scanButton = (Button) findViewById(R.id.scan_button);
-        imageView = (ImageView) findViewById(R.id.qr_code);
-
-        /*
-        scanButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), ScanActivity.class));
-            }
-        });
-        */
-
-        scanButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new QR(imageView).execute("https://api.qr-code-generator.com/v1/create/" + test);
-            }
-        });
     }
 }
