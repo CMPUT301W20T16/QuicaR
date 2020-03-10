0bde5ee58c11576d8ed62fbba7a64b333b907dc3
claire45 <yliu17@ualberta.ca>
2020-03-09 21:39:32 -0600

    updated map merged with database and mapView

---
+++ QuicaR 4/app/src/main/java/com/example/quicar/RiderSelectLocationActivity.java
@@ -0,0 +1,231 @@
+package com.example.quicar;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+//import com.example.quicar_mapview.R;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
+public class RiderSelectLocationActivity extends AppCompatActivity{
+    private EditText pickUp;
+    private EditText destination;
+    private Button confirmButton;
+
+
+    String address,locality,subLocality,state,postalCode,country,knownname,phone;
+//    TextView txtaddress, txtlocality, txtsubLocality, txtstate,txtpostalCode,txtcountry,txtknownname,txtphone;
+    private double currentLat,currentLng;
+    private Location start_location, end_location;
+
+//    private GoogleMap mMap;
+
+
+    Marker marker;
+    PlacesClient placesClient;
+
+    AutocompleteSupportFragment pickUpAutoComplete;
+    AutocompleteSupportFragment destinationAutoComplete;
+
+
+
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rider_select_location);
+
+        // add back button on action bar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+
+        String apiKey= "AIzaSyCyECZAmZ2NxQz10Qijm-ngagqBdHJblzk";
+        if (!Places.isInitialized()){
+            Places.initialize(getApplicationContext(),apiKey);
+        }
+
+        placesClient = Places.createClient(this);
+
+
+        start_location = new Location();
+        end_location = new Location();
+
+
+
+        AutocompleteSupportFragment pickUpAutoComplete =
+                (AutocompleteSupportFragment) getSupportFragmentManager().findFragmentById(R.id.pick_up);
+
+        AutocompleteSupportFragment destinationAutoComplete =
+                (AutocompleteSupportFragment) getSupportFragmentManager().findFragmentById(R.id.destination);
+
+        //get data from intent, i.e., current address
+        Intent intent = getIntent();
+        String pick_up_address = (String) intent.getSerializableExtra("current pos");
+        start_location = (Location) intent.getSerializableExtra("current location");
+
+
+        pickUpAutoComplete.setHint(pick_up_address);
+        destinationAutoComplete.setHint("Select Destination");
+
+        onCreateAutoCompletion(pickUpAutoComplete, start_location);
+        onCreateAutoCompletion(destinationAutoComplete, end_location);
+
+
+    }
+
+
+    public void onCreateAutoCompletion(final AutocompleteSupportFragment autocompleteSupportFragment, final Location location) {
+        autocompleteSupportFragment.setPlaceFields(
+                Arrays.asList(
+                        Place.Field.ID,
+                        Place.Field.LAT_LNG,
+                        Place.Field.PHONE_NUMBER,
+                        Place.Field.ADDRESS
+                )
+        );
+
+        autocompleteSupportFragment.setOnPlaceSelectedListener(
+                new PlaceSelectionListener() {
+                    @Override
+                    public void onPlaceSelected(@NonNull Place place) {
+                        LatLng latLng = place.getLatLng();
+                        currentLat = latLng.latitude;
+                        currentLng = latLng.longitude;
+                        //如果user没有更新start lcoation的情况
+//                        if (currentLat == 0.0f || currentLng == 0.0f){
+//                            return;
+//                        }
+                        location.setLat(currentLat);
+                        location.setLon(currentLng);
+//                        DatabaseHelper.setSecondLocation(new Location(currentLat, currentLng));
+//                        User newUser = new User();
+//                        newUser.setName("testing1");
+//                        Request request = new Request(new Location(), DatabaseHelper.getSecondLocation(),
+//                                newUser, new User(), 89.f);
+//                        RequestDataHelper.addNewRequest(request, listener);
+
+                        phone = place.getPhoneNumber();
+                        address = place.getAddress();
+
+//                        if ( marker != null){
+//                            marker.remove();
+//                        }
+
+//                        mMap.clear();
+//                        marker = mMap.addMarker(new MarkerOptions().position(latLng).title(place.getName()));
+//                        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,16.5f ),null);
+
+
+                        Geocoder gcd =  new Geocoder(getBaseContext(), Locale.getDefault());
+
+                        List<Address> addresses;
+
+                        try {
+                            addresses = gcd.getFromLocation(currentLat,currentLng,1);
+                            if (addresses.size()> 0){
+                                locality = addresses.get(0).getLocality();
+                                //ystem.out.println("\n\n aaaaaaaaaaaa");
+                                //autocompleteSupportFragment.setText(locality);
+                                EditText etPlace = (EditText) autocompleteSupportFragment
+                                        .getView()
+                                        .findViewById(R.id.places_autocomplete_search_input);
+                                etPlace.setHint(place.getAddress());
+                                //System.out.println(destinationAutoComplete);
+
+
+                            }
+
+
+                        }catch (IOException e){
+                            e.printStackTrace();
+                        }
+
+
+
+
+                    }
+
+                    @Override
+                    public void onError(@NonNull Status status) {
+
+                    }
+                }
+        );
+    }
+
+
+    /**
+     * listview包含近10(?)个rider去过的location
+*/
+
+
+    // Used to add check bar on top right of the app bar.
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.action_bar_confirm, menu);
+        return true;
+    }
+
+
+     // When user clicks on the tick button, this function checks if any of the entries are left blank.
+    // If so, a Toast object is used to notify that the
+    // user left a field empty. Otherwise, we add the measurement.
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.confirm_button:
+                // check if inputs are left blank
+                if (start_location.getLat() == null || end_location.getLat() == null){
+                    Toast.makeText(this, "One or more fields is empty!", Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+                Intent intent = new Intent(RiderSelectLocationActivity.this, RiderConfirmRiderActivity.class);
+                intent.putExtra("start location", start_location);
+                intent.putExtra("end location", end_location);
+                startActivity(intent);
+                return true;
+
+            default:
+                // if we got here, the user's action was not recognized.
+                // invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+}
+
---
+++ QuicaR 4/app/src/main/java/com/example/quicar/RiderWaitingRideActivity.java
@@ -0,0 +1,154 @@
+package com.example.quicar;
+
+import android.app.Notification;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+
+import java.util.ArrayList;
+
+import static com.example.quicar.App.CHANNEL_1_ID;
+
+
+/**
+ * rider has successfully sent out a request
+ * rider is waiting the system to pair them with a driver
+ * rider will get a notification if the match is successful
+ * rider is able to cancel ride before driver arrived at pick up location
+ * (rider can only cancel ride in a reasonable amount of time)
+ */
+
+public class RiderWaitingRideActivity extends BaseActivity implements OnGetRequestDataListener{
+
+    LinearLayout linearLayout;
+    BottomSheetBehavior bottomSheetBehavior;
+
+    TextView driverDistance;
+    TextView driverName;
+    TextView driverEmail;
+    TextView driverPhone;
+
+    Button DetailButton;
+    Button CallButton;
+    Button EmailButton;
+    Button CancelButton;
+
+    private NotificationManagerCompat notificationManager;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        View rootView = getLayoutInflater().inflate(R.layout.activity_rider_waiting_ride, frameLayout);
+
+//        Toast.makeText(RiderWaitingRideActivity.this, "rider request updated to active by driver!!!", Toast.LENGTH_SHORT).show();
+        System.out.println("****** enter");
+        RequestDataHelper.setOnNotifyListener(this);
+
+
+        notificationManager = NotificationManagerCompat.from(this);
+
+        linearLayout = (LinearLayout) findViewById(R.id.bottom_sheet_ride_status);
+        bottomSheetBehavior = BottomSheetBehavior.from(linearLayout);
+
+        // set Buttons
+        DetailButton = linearLayout.findViewById(R.id.driver_detail_button);
+        CallButton = linearLayout.findViewById(R.id.call_driver_button);
+        EmailButton = linearLayout.findViewById(R.id.email_driver_button);
+
+        // set TextView
+        driverName = linearLayout.findViewById(R.id.driver_name_tv);
+        driverEmail = linearLayout.findViewById(R.id.driver_email_tv);
+        driverPhone = linearLayout.findViewById(R.id.driver_phone_tv);
+        driverDistance = linearLayout.findViewById(R.id.driver_distance_tv);
+
+
+        // set on click listener for buttons
+        // transfer to default dial page
+        CallButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:" + driverPhone.getText().toString()));
+                startActivity(intent);
+
+            }
+        });
+
+        //send email需要拿到user的email，格式类似上面打电话
+
+        /**
+         * if successfully matched, call sendOnChannel1
+         * While driver is browsing for open requests
+         */
+
+    }
+
+        // used to send one notification
+        public void sendOnChannel1(View v) {
+            String title = "Get Ready!";
+            String message = "Your driver is on the way!";
+
+            Notification notification = new NotificationCompat.Builder(this, CHANNEL_1_ID)
+                    .setSmallIcon(R.drawable.ic_cancel)
+                    .setContentTitle(title)
+                    .setContentText(message)
+                    .setPriority(NotificationCompat.PRIORITY_HIGH)
+                    .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                    .build();
+
+            notificationManager.notify(1, notification);
+        }
+
+    @Override
+    public void onSuccess(ArrayList<Request> requests, String tag) {
+
+
+    }
+
+    @Override
+    public void onActiveNotification(Request request) {
+        Toast.makeText(RiderWaitingRideActivity.this, "rider request updated to active by driver!!!", Toast.LENGTH_SHORT).show();
+       DatabaseHelper.sendPopUpNotification("active request notification","testing",this);
+
+
+    }
+
+    @Override
+    public  void onPickedUpNotification(Request request) {
+
+    }
+
+    @Override
+    public void onCancelNotification() {
+
+    }
+
+    @Override
+    public void onCompleteNotification() {
+
+    }
+
+    @Override
+    public void onFailure(String errorMessage, String tag) {
+
+
+    }
+
+
+
+}
