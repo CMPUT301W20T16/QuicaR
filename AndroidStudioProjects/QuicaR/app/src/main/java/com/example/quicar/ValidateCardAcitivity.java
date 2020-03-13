6b01678175700f18964ec815ed28fa33ffbab94c
PANGK4IK4I <wenkai1@ualberta.ca>
2020-03-13 11:31:33 -0600

    Merge branches 'Jeremy' and 'pangkaikai' of https://github.com/CMPUT301W20T16/QuicaR into pangkaikai
    # Conflicts:
    #	AndroidStudioProjects/QuicaR/app/build.gradle
    #	AndroidStudioProjects/QuicaR/app/src/main/AndroidManifest.xml
    #	AndroidStudioProjects/QuicaR/app/src/main/java/com/example/quicar/BaseActivity.java
    #	AndroidStudioProjects/QuicaR/app/src/main/java/com/example/quicar/MainActivity.java
    #	AndroidStudioProjects/QuicaR/app/src/main/java/com/example/quicar/QR.java

--- AndroidStudioProjects/QuicaR/app/src/main/java/com/example/quicar/ValidateCardAcitivity.java
+++ AndroidStudioProjects/QuicaR/app/src/main/java/com/example/quicar/ValidateCardAcitivity.java
@@ -32,30 +32,39 @@ public class ValidateCardAcitivity extends AppCompatActivity {
         ccv = (EditText)findViewById(R.id.ccv) ;
 
         BankAccount card = (BankAccount) getIntent().getSerializableExtra("card");
-        name.setText(card.getNameOnCard());
-        cardNum.setText(card.getCardnumber());
-        expiryDate.setText(card.getExpireDate().toString());
-        ccv.setText(card.getCcvCode());
+//        name.setText(card.getNameOnCard());
+//        cardNum.setText(card.getCardnumber());
+//        expiryDate.setText(card.getExpireDate().toString());
+//        ccv.setText(card.getCcvCode());
 
         confirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent();
-                BankAccount card_1 = new BankAccount();
-                card_1.setNameOnCard(name.getText().toString());
-                card_1.setCardnumber(cardNum.getText().toString());
-                card_1.setCcvCode(ccv.getText().toString());
-                card_1.setType(null);
-                String Date = expiryDate.getText().toString();
-                try {
-                    Date date = new SimpleDateFormat("yyyy/MM/dd").parse(Date);
-                    card_1.setExpireDate(date);
-                } catch (ParseException e) {
-                    e.printStackTrace();
+                if (cardNum.getText().toString().length() != 16){
+                    Toast.makeText(ValidateCardAcitivity.this,"Please enter valid card number with length 16",Toast.LENGTH_SHORT ).show();
                 }
-                intent.putExtra("card_1", card_1);
-                setResult(RESULT_OK, intent);
-                finish();
+                else if (name.getText().toString().length() == 0 || ccv.getText().toString().length() == 0
+                        || expiryDate.getText().toString().length() == 0){
+                    Toast.makeText(ValidateCardAcitivity.this,"Please fill all the information",Toast.LENGTH_SHORT ).show();
+                }
+                else {
+                    Intent intent = new Intent();
+                    BankAccount card_1 = new BankAccount();
+                    card_1.setNameOnCard(name.getText().toString());
+                    card_1.setCardnumber(cardNum.getText().toString());
+                    card_1.setCcvCode(ccv.getText().toString());
+                    card_1.setType(null);
+                    String Date = expiryDate.getText().toString();
+                    try {
+                        Date date = new SimpleDateFormat("yyyy/MM/dd").parse(Date);
+                        card_1.setExpireDate(date);
+                    } catch (ParseException e) {
+                        Toast.makeText(ValidateCardAcitivity.this,"Please fill the date in the format yyyy/MM/dd",Toast.LENGTH_SHORT ).show();
+                    }
+                    intent.putExtra("card_1", card_1);
+                    setResult(RESULT_OK, intent);
+                    finish();
+                }
             }
         });
     }
