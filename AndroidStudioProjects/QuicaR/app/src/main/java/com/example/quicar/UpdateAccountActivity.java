8d79b67bf38133c281e9aafa5c8aa4e7ab724a8d
Zhiyuan <zlu6@ualberta.ca>
2020-03-28 19:42:33 -0600

    add email/password update methods

--- AndroidStudioProjects/QuicaR/app/src/main/java/com/example/quicar/UpdateAccountActivity.java
+++ AndroidStudioProjects/QuicaR/app/src/main/java/com/example/quicar/UpdateAccountActivity.java
@@ -16,6 +16,7 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.example.datahelper.DatabaseHelper;
 import com.example.datahelper.UserDataHelper;
 import com.example.listener.OnGetUserDataListener;
 import com.example.user.User;
@@ -38,6 +39,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class UpdateAccountActivity extends AppCompatActivity {
+    private static final String TAG = "UpdateAccountActivity";
 
 
     @Override
@@ -46,7 +48,7 @@ public class UpdateAccountActivity extends AppCompatActivity {
         setContentView(R.layout.activity_update_account);
         ListView list = (ListView) findViewById(R.id.update_username);
         ArrayList<String> optionList = new ArrayList<>();
-        optionList.add("Change Username");
+        optionList.add("Change Email");
         optionList.add("Change Password");
 
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, optionList);
@@ -59,57 +61,63 @@ public class UpdateAccountActivity extends AppCompatActivity {
                     AlertDialog.Builder builder = new AlertDialog.Builder(UpdateAccountActivity.this);
                     LayoutInflater inflater = LayoutInflater.from(UpdateAccountActivity.this);
                     View viewDialog = inflater.inflate(R.layout.username_update_dialog, null);
-                    EditText username_change = viewDialog.findViewById(R.id.username_change);
+                    EditText email_change = viewDialog.findViewById(R.id.email_change);
                     EditText pwd_confirm = viewDialog.findViewById(R.id.password_check);
                     builder.setView(viewDialog);
-                    builder.setTitle("change username");
+                    builder.setTitle("change email");
                     builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
-                            String newUserName = username_change.getText().toString();
+                            String newEmail = email_change.getText().toString();
                             String checkPwd = pwd_confirm.getText().toString();
                             FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
                             // Get auth credentials from the user for re-authentication. The example below shows
                             // email and password credentials but there are multiple possible providers,
                             // such as GoogleAuthProvider or FacebookAuthProvider.
+                            String oldEmail = FirebaseAuth.getInstance().getCurrentUser().getEmail();
                             AuthCredential credential = EmailAuthProvider
-                                    .getCredential(FirebaseAuth.getInstance().getCurrentUser().getEmail(), checkPwd);
+                                    .getCredential(oldEmail, checkPwd);
 
                             // Prompt the user to re-provide their sign-in credentials
                             user.reauthenticate(credential)
                                     .addOnCompleteListener(new OnCompleteListener<Void>() {
                                         @Override
                                         public void onComplete(@NonNull Task<Void> task) {
-                                            // Log.d(TAG, "User re-authenticated.");
-                                            FirebaseAuth.getInstance().getCurrentUser().updateEmail(newUserName).addOnCompleteListener(new OnCompleteListener<Void>() {
-                                                @Override
-                                                public void onComplete(@NonNull Task<Void> task) {
-                                                    if (task.isSuccessful()) {
-                                                        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("User");
-                                                        String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+                                            if (task.isSuccessful()) {
+                                                Log.d(TAG, "Re-auth success");
+                                                FirebaseAuth.getInstance().getCurrentUser().updateEmail(newEmail).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                    @Override
+                                                    public void onComplete(@NonNull Task<Void> task) {
+                                                        if (task.isSuccessful()) {
+                                                            DatabaseReference ref = FirebaseDatabase.getInstance().getReference("User");
+                                                            String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
-                                                        ref.child(uid).child("accountInfo").child("email").setValue(newUserName);
-                                                        Query query = ref.orderByChild("AccountInfo/email").equalTo(newUserName);
-                                                        query.addListenerForSingleValueEvent(new ValueEventListener() {
-                                                            @Override
-                                                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                                                                for (DataSnapshot dataSnapshot1:dataSnapshot.getChildren()) {
-                                                                    User myUser = dataSnapshot1.getValue(User.class);
-                                                                    myUser.getAccountInfo().setEmail(newUserName);
+                                                            ref.child(uid).child("accountInfo").child("email").setValue(newEmail);
+                                                            Query query = ref.orderByChild("AccountInfo/email").equalTo(newEmail);
+                                                            query.addListenerForSingleValueEvent(new ValueEventListener() {
+                                                                @Override
+                                                                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                                                    for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
+                                                                        User myUser = dataSnapshot1.getValue(User.class);
+                                                                        myUser.getAccountInfo().setEmail(newEmail);
+                                                                    }
                                                                 }
-                                                            }
 
-                                                            @Override
-                                                            public void onCancelled(@NonNull DatabaseError databaseError) {
+                                                                @Override
+                                                                public void onCancelled(@NonNull DatabaseError databaseError) {
 
-                                                            }
-                                                        });
+                                                                }
+                                                            });
 
-                                                        Toast.makeText(getApplicationContext(), "email updated successful", Toast.LENGTH_SHORT).show();
+                                                            Toast.makeText(getApplicationContext(), "email updated successful", Toast.LENGTH_SHORT).show();
+                                                        }
                                                     }
-                                                }
-                                            });
+                                                });
+                                            } else {
+                                                Log.d(TAG, "re-auth failed");
+                                                Toast.makeText(getApplicationContext(), "wrong password", Toast.LENGTH_SHORT).show();
+                                            }
                                         }
                                     });
                             // String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
@@ -138,6 +146,47 @@ public class UpdateAccountActivity extends AppCompatActivity {
                         }
                     });
                     builder.create().show();
+                }
+
+                if (position == 1) {
+                    AlertDialog.Builder builderPwd = new AlertDialog.Builder(UpdateAccountActivity.this);
+                    LayoutInflater inflaterPwd = LayoutInflater.from(UpdateAccountActivity.this);
+                    View viewDialogPwd = inflaterPwd.inflate(R.layout.password_update_dialog, null);
+                    EditText mOriginEmail = viewDialogPwd.findViewById(R.id.origin_pwd);
+                    EditText mpwdUpdate = viewDialogPwd.findViewById(R.id.pwd_dialog_change);
+                    EditText mpwdConfirm = viewDialogPwd.findViewById(R.id.pwd_confirm_dialog);
+                    builderPwd.setView(viewDialogPwd);
+                    builderPwd.setTitle("Change Password");
+                    builderPwd.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            String originEmail = mOriginEmail.getText().toString();
+                            String pwdReset = mpwdUpdate.getText().toString();
+                            String pwdResetConfirm = mpwdConfirm.getText().toString();
+                            FirebaseUser mUser = FirebaseAuth.getInstance().getCurrentUser();
+
+
+                        // Get auth credentials from the user for re-authentication. The example below shows
+                        // email and password credentials but there are multiple possible providers,
+                        // such as GoogleAuthProvider or FacebookAuthProvider.
+                            AuthCredential credential = EmailAuthProvider
+                                    .getCredential(mUser.getEmail(), originEmail);
+
+                        // Prompt the user to re-provide their sign-in credentials
+                            mUser.reauthenticate(credential)
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()) {
+                                                Log.d(TAG, "User re-authenticated.");
+                                                Toast.makeText(getApplicationContext(), "re-authenticated", Toast.LENGTH_SHORT).show();;
+
+                                            }
+                                        }
+                                    });
+                        }
+                    });
+                    builderPwd.create().show();
                 }
 
             }
