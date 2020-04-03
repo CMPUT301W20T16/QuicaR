5cde67755e4861692a2ae4955ebb5f86a654d034
youwei1-sudo <davidchenyouwei1999@gmail.com>
2020-04-03 12:32:31 -0600

    Register Activity Test

---
+++ AndroidStudioProjects/QuicaR/app/src/test/java/com/example/quicar/PayHistoryTest.java
@@ -0,0 +1,25 @@
+package com.example.quicar;
+
+import android.content.Context;
+
+import com.example.entity.PayRecord;
+import com.example.user.AccountInfo;
+import com.example.user.PayHistoryList;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+public class PayHistoryTest {
+//    private PayHistoryList mockPayHitory() {
+//
+//        ArrayList<PayRecord> payRecords = new ArrayList<>();
+//        Context context = new Context();
+//        PayHistoryList historyList = new PayHistoryList(payRecords,context);
+//        return historyList;
+//    }
+    @Test
+    public void checkGetter() {
+        ;
+    }
+}
